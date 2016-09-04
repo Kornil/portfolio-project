42c8d23836e615dd2cd06e83ad1c3a50bf338ba8
Francesco Agnoletto <Francesco.agnoletto92@gmail.com>
2016-09-04 19:39:50 +0200

    change color

--- js/base.js
+++ js/base.js
@@ -1,12 +1,12 @@
 var HelloMessage = React.createClass({
-          render: function() {
-            return (<div>
-                        <h1>Hello {this.props.name}</h1>
-                    </div>)
-          }
-        });
+  render: function() {
+    return (<div>
+      <h1>Hello {this.props.name}</h1>
+    </div>)
+  }
+});
 
-        ReactDOM.render(
-          <HelloMessage name="Francesco" />,
-          document.getElementById('container')
-        );
+ReactDOM.render(
+  <HelloMessage name="Francesco" />,
+  document.getElementById('container')
+);
