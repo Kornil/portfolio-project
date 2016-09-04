3ac24bd22fe218db57e555b6d8c4433531b09edc
Francesco Agnoletto <Francesco.agnoletto92@gmail.com>
2016-09-04 19:28:55 +0200

    test again

--- js/base.js
+++ js/base.js
@@ -1,4 +1,12 @@
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('example')
-);
+var HelloMessage = React.createClass({
+          render: function() {
+            return (<div>
+                        <h1>Hello {this.props.name}</h1>
+                    </div>)
+          }
+        });
+
+        ReactDOM.render(
+          <HelloMessage name="Francesco" />,
+          document.getElementById('container')
+        );
