32c7fb59a0301c89064e382258e8676919e4e0e6
Francesco Agnoletto <Francesco.agnoletto92@gmail.com>
2016-09-04 20:31:05 +0200

    react-bootstrap test

--- js/base.js
+++ js/base.js
@@ -1,6 +1,12 @@
 var HelloMessage = React.createClass({
   render: function() {
     return (
+    	<Grid>
+		    <Row className="show-grid">
+		      <Col xs={12} md={8}>Hello {this.props.name}</Col>
+		      <Col xs={12} md={4}>Im green</Col>
+		    </Row>
+		  </Grid>
         <div>
     	    <div id="container">
     	      Hello {this.props.name}
