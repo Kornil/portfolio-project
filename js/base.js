14ad9b9c893cda5c047221396a87fd776f689719
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2016-09-06 16:23:30 +0200

    react setState test

--- js/base.js
+++ js/base.js
@@ -3,25 +3,36 @@ var Row = ReactBootstrap.Row;
 var Col = ReactBootstrap.Col;
 let text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
-var HelloMessage = React.createClass({
+var Stats = React.createClass({
+	render: function(){
+		return (
+			<Col xs={12} md={3} id="stats">
+      	<div id="circle"></div>
+      	<h3>{this.props.test}</h3>
+      	<p>{text}</p>
+      </Col>
+			)
+	}
+});
+
+var FinalRender = React.createClass({
+	getInitialState: function() {
+    return {test: 0};
+  },
   render: function() {
     return (
     	<Grid>
 		    <Row className="show-grid">
-		      <Col xs={12} md={9} id="bg">
+		      <Col onClick={this.setState(test: this.state.test + 1)} xs={12} md={9} id="bg">
 		      	<h1>{this.props.name} Agnoletto</h1>
 		      </Col>
-		      <Col xs={12} md={3} id="stats">
-		      	<div id="circle"></div>
-		      	<h3>title</h3>
-		      	<p>{text}</p>
-		      </Col>
+		      <Stats test={this.state.test}/>
 		    </Row>
 		  </Grid>)
   }
 });
 
 ReactDOM.render(
-  <HelloMessage name="Francesco" />,
+  <FinalRender name="Francesco" />,
   document.getElementById('wrapper')
 );
