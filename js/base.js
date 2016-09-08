1a207018997422061600043a78edfb85f74213da
Francesco Agnoletto <Francesco.agnoletto92@gmail.com>
2016-09-09 01:21:30 +0200

    test hover 2

--- js/base.js
+++ js/base.js
@@ -67,7 +67,7 @@ var FinalRender = React.createClass({
 		      	<h1 onClick={this.handleClick}>Francesco Agnoletto</h1>
 		      	<div id="icons">
 		      		{statsText.map(function(object, i){
-				        return <i onMouseover={this.hoverEvent(i)} className={object.icon} key={i} height="45px" />;
+				        return <i onMouseover={()=>this.hoverEvent(i)} className={object.icon} key={i} height="45px" />;
 				    	})}
 		      	</div>
 		      </Col>
