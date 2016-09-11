2af858beed9d17f0f11fdb260fb1c3d1efb32ea6
Francesco Agnoletto <Francesco.agnoletto92@gmail.com>
2016-09-11 09:38:57 +0200

    test remove blank icons

--- js/base.js
+++ js/base.js
@@ -62,6 +62,7 @@ var FinalRender = React.createClass({
 		      	</div>
 		      	<div id="icons">
 		      		{statsText.map(function(object, i){
+		      			if(object.icon === null){return}
 				        return <i onMouseOver={this.handleHover.bind(this, i)} onMouseOut={this.handleHover.bind(this, 0)} className={object.icon} key={i} height="45px" />;
 				    	}.bind(this))}
 		      	</div>
